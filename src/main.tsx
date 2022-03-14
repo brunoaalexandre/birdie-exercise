1d0da86db75fb7d929d02a9e11ac7e57834e37a3
Bruno Alexandre <brunoalencaralexandre@gmail.com>
2022-03-14 12:35:06 -0300

    Add header and new sessions component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+
+import { GlobalStyles } from "./styles/global";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyles />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
